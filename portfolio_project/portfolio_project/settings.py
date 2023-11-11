71bd280af53d3acf02af6c30dc705a1ac2783bfa
reinikeda <reinikeda@gmail.com>
2023-11-11 21:02:40 +0200

    settings fix allowed host

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['daivareinike.herokuapp.com']
 
 
 # Application definition
